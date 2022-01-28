7f4986ee31ac34efbd6630605cae8f23864b7bdd
Zhibek Shakenova <zhibek.shakenovva.on@gmail.com>
2022-01-28 16:39:15 +0600

    add wrapper

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.body.style.background ="rgb(18, 25, 61)";
+
+const wrapper = document.createElement("div");
+wrapper.id = "wrapper";
+document.body.append(wrapper);
+wrapper.style.width = "375px";
+wrapper.style.fontFamily = '"Roboto Condensed", sans-serif';
+wrapper.style.height = "610px";
+wrapper.style.backgroundColor = "rgb(25, 45, 75)";
+wrapper.style.margin = "auto";
+wrapper.style.borderRadius = "20px";
+wrapper.style.marginTop = "100px";
+wrapper.style.marginBottom = "100px";
+
