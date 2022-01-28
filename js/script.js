ae834999383aa1dc18a28d28413af86004841dcf
Zhibek Shakenova <zhibek.shakenovva.on@gmail.com>
2022-01-28 17:59:47 +0600

    add img

--- js/script.js
+++ js/script.js
@@ -5,10 +5,25 @@ wrapper.id = "wrapper";
 document.body.append(wrapper);
 wrapper.style.width = "375px";
 wrapper.style.fontFamily = '"Roboto Condensed", sans-serif';
-wrapper.style.height = "610px";
+wrapper.style.height = "590px";
 wrapper.style.backgroundColor = "rgb(25, 45, 75)";
 wrapper.style.margin = "auto";
-wrapper.style.borderRadius = "20px";
+wrapper.style.borderRadius = "15px";
 wrapper.style.marginTop = "100px";
 wrapper.style.marginBottom = "100px";
+
+// const wrapper2 = wrapper.cloneNode;
+// wrapper2 = "wrapper2";
+// document.body.appendChild(wrapper2);
+
+//image
+
+const image = document.createElement ("img");
+wrapper.append(image);
+image.setAttribute("src", "images/image-equilibrium.jpg");
+image.style.width = "335px";
+image.style.height = "300px";
+image.style.borderRadius = "15px";
+image.style.marginLeft = "20px";
+image.style.marginTop = "20px";
 
